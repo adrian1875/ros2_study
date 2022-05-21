08e0ff142b573f9b3a4b24a4c1c8f62e995559be
choi <choiyh1875@gmail.com>
2022-05-21 12:28:03 +0000

    22.05.21 Topic end

--- src/gcamp_ros2_basic/cpp_topic_pkg/src/laser_sub.cpp
+++ src/gcamp_ros2_basic/cpp_topic_pkg/src/laser_sub.cpp
@@ -24,23 +24,27 @@ private:
 
 public:
   LaserSub() : Node("topic_sub_oop_node") {
+    // important!! make subscriber
     m_sub = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "skidbot/scan", 10,
+        // sub_callback must have one or more parameter, because parameters are datas of subscribe
         std::bind(&LaserSub::sub_callback, this, std::placeholders::_1));
   }
-
+  // SharedPtr is smart pointer
   void sub_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+    // ranges 배열이 기존 Python에서는 list 형식으로 표현되었지만, C++에서는 vector로 표현된다는 점에서 차이가 있습니다.
     // std::cout << (msg->ranges).size() << std::endl;
 
     // for (auto e : msg->ranges)
     //   std::cout << e << std::endl;
-
+    // msg->ranges : 0 ~ 720, 360 : front 
     RCLCPP_INFO(this->get_logger(), "Distance from Front Object : %f", (msg->ranges)[360]);
   }
 };
 
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
+  // subscriber는 반드시 spin을 통한 갱신이 있어야 최신 message를 수신할 수 있다는 점을 다시 한 번 말씀드립니다.
   rclcpp::spin(std::make_shared<LaserSub>());
   rclcpp::shutdown();
 
